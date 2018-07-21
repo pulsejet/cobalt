aebe78477a2ab50cc42f98ac10b659dd73a654b3
Varun Patil <radialapps@gmail.com>
2018-07-21 23:02:25 +0530

    Fix login url

--- cobalt/settings.py
+++ cobalt/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/cobalt/accounts/login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
